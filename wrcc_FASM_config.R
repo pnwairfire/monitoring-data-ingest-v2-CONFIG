2a75904233705173869f16c099506684e0be0982
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2024-06-20 07:10:23 -0700

    Update wrcc_FASM_config.R

--- wrcc_FASM_config.R
+++ wrcc_FASM_config.R
@@ -25,6 +25,7 @@ WRCC_FASM_EXCLUDE_BY_deviceDeploymentID <-
     "4d39246298384298_wrcc.1307",
     "0d330fab658f7144_wrcc.sm68",
     "9tuver3007_wrcc.eat1",
+    "8537af3a2db7bfe0_wrcc.sm11",
     "DONT_FIND_ME" # KEEP THIS to ensure we end with no comma
   )
 
