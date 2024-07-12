0f4dd57edee4ee117fc947fc12187542df442968
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2024-07-12 13:33:05 -0700

    Update airsis_FASM_config.R

--- airsis_FASM_config.R
+++ airsis_FASM_config.R
@@ -25,6 +25,7 @@ AIRSIS_FASM_EXCLUDE_BY_deviceDeploymentID <-
     # "a-first-id",
     # "a-second-id",
     "9r34s9xkvs_arb2.1034",
+    "cad03848fefb10a1_arb2.1035",
     "DONT_FIND_ME" # KEEP THIS to ensure we end with no comma
   )
 
